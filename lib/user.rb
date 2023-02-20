ce27873e87300de0fbac781d7b15065d646aa391
WendyNjuguna <wendynjuguna714@gmail.com>
2023-02-20 10:45:47 +0300

    commit tree 8e5b57556c62f7cbed2a29768169cc1841e8a6ad

---
+++ lib/user.rb
@@ -0,0 +1,24 @@
+class User 
+    attr_accessor :first_name, :last_name
+   # first_name
+   # setter method
+   def first_name=(first_name)
+       @first_name = first_name
+       # @knowledge.push(first_name)
+   end
+   # getter method
+   def first_name
+       @first_name
+   end
+
+   # last_name
+   # setter method
+   def last_name=(last_name)
+       @last_name = last_name
+   end
+   
+   def last_name
+       @last_name
+   end
+
+end    
